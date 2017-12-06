76e08be3f33a18d5fa09f2a413406005d7b04c9c
sophiacaspar <sophiacaspar@hotmail.com>
2017-12-06 20:19:33 +0100

    last fixes on part 1 before continuing with part 2

--- AdventOfCode17.Day1/AdventOfCode17.Host/Program.cs
+++ AdventOfCode17.Day1/AdventOfCode17.Host/Program.cs
@@ -11,7 +11,6 @@ namespace AdventOfCode17.Host
             string pathToFile = @"C:\Users\sophia.caspar\source\repos\AdventOfCode17\AdventOfCode17.Day1\puzzleInput.txt";
             var fileReader = new FileReader(pathToFile);
             string sequence = fileReader.GetInputFromFile();
-
             var captcha = new Captcha();
             captcha.ReviewSequence(sequence);
             Console.WriteLine(captcha.GetSum());
